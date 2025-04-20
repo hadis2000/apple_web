b0b8b19039af22fcfe555d380fe401d54b4e1ea1
hadis2000 <hadisehoseini6301@gmail.com>
2025-04-20 15:07:26 +0330

    .

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,12 +1,13 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import ModelView from "./ModelView";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { yellowImg } from "../utils";
 import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
 import { models, sizes } from "../constants";
+import { animateWithGsapTimeLine } from "../utils/animations";
 
 const Model = () => {
   const [size, setSize] = useState("small");
@@ -27,6 +28,24 @@ const Model = () => {
   // rotation
   const [smallRotation, setSmallRotation] = useState(0);
   const [largeRotation, setLargeRotation] = useState(0);
+
+  const tl = gsap.timeline();
+
+  useEffect(() => {
+    if (size === "large") {
+      animateWithGsapTimeLine(tl, small, smallRotation, "#view1", "#view2", {
+        transform: "translateX(-100%)",
+        duration: 2,
+      });
+    }
+
+    if (size === "small") {
+      animateWithGsapTimeLine(tl, large, largeRotation, "#view2", "#view1", {
+        transform: "translateX(0)",
+        duration: 2,
+      });
+    }
+  }, [size]);
 
   useGSAP(() => {
     gsap.to("#heading", { y: 0, opacity: 1 });
@@ -50,7 +69,7 @@ const Model = () => {
               size={size}
             />
 
-            {/* <ModelView
+            <ModelView
               index={2}
               groupRef={large}
               gsapType="view2"
@@ -58,7 +77,7 @@ const Model = () => {
               setRotationState={setLargeRotation}
               item={model}
               size={size}
-            /> */}
+            />
 
             <Canvas
               className="w-full h-full"
